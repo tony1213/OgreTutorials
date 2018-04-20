557a36bf35158c0db89c1db657c0eb4ad62a2933
Chen Rui <chenr@partnerx.com>
2018-04-20 17:31:46 +0800

    modify transform function

--- coordinate_transform.cpp
+++ coordinate_transform.cpp
@@ -15,11 +15,38 @@ CoordinateTransform::~CoordinateTransform()
 
 bool CoordinateTransform::getTransform(const std::string& frame, QDateTime time, Ogre::Vector3& position, Ogre::Quaternion& orientation){
 
+    if ( !adjustTime(frame, time) )
+    {
+        return false;
+    }
+    boost::mutex::scoped_lock lock(cache_mutex_);
+
+    position = Ogre::Vector3(9999999, 9999999, 9999999);
+    orientation = Ogre::Quaternion::IDENTITY;
+
+    if (fixed_frame_.empty())
+    {
+        return false;
+    }
+
+    M_Cache::iterator it = cache_.find(CacheKey(frame, time));
+    if (it != cache_.end())
+    {
+        position = it->second.position;
+        orientation = it->second.orientation;
+        return true;
+    }
+    Ogre::Vector3 positionX; 
+    Ogre::Quaternion orientationY; 
+    orientationY.w = 1.0f;
 
 
+    if (!transform(frame, time, positionX, orientationY, position, orientation))
+    {
+        return false;
+    }
 
-
-
+    cache_.insert(std::make_pair(CacheKey(frame, time), CacheEntry(position, orientation)));
 
 
     return true;
