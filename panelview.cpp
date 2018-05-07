932306371c37283dce28f5e8b0a4cd9810ecfa6a
Wu Dong <wud@partnerx.com>
2018-05-07 17:04:34 +0800

    merge from chenrui

--- panelview.cpp
+++ panelview.cpp
@@ -92,25 +92,259 @@ void PanelView::initVariablesUI(){
         var[i] = new QSpinBox(this);
         var[i]->setGeometry(QRect(250, height, 100, 20));
 
-        slider[i] = new QSlider(this);
+       // slider[i] = new QSlider(this);
+        slider[i] = new MyCustomSlider(this);
         slider[i]->setOrientation(Qt::Horizontal);
         slider[i]->setGeometry(QRect(360, height, 150, 20));
 
         //here, perhaps different var has different range value
         var[i]->setRange(0, 130);  
         slider[i]->setRange(0, 130);  
-        //
                  
-        QObject::connect(var[i], SIGNAL(valueChanged(int)),
-                    slider[i], SLOT(setValue(int)));  
-        QObject::connect(slider[i], SIGNAL(valueChanged(int)),
-                    var[i], SLOT(setValue(int))); 
-
 
     }
 
+        QObject::connect(slider[0], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle0(int))); 
+        QObject::connect(slider[1], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle1(int)));
+        QObject::connect(slider[2], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle2(int)));
+        QObject::connect(slider[3], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle3(int)));
+        QObject::connect(slider[4], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle4(int)));
+        QObject::connect(slider[5], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle5(int)));
+        QObject::connect(slider[6], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle6(int)));
+        QObject::connect(slider[7], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle7(int)));
+        QObject::connect(slider[8], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle8(int)));
+        QObject::connect(slider[9], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle9(int)));
+        QObject::connect(slider[10], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle10(int)));
+        QObject::connect(slider[11], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle11(int)));
+        QObject::connect(slider[12], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle12(int)));
+        QObject::connect(slider[13], SIGNAL(valueChanged(int)),
+                    this, SLOT(updateRobotAngle13(int)));
+
+
+        QObject::connect(var[0], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar0(int)));
+        QObject::connect(var[1], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar1(int)));
+        QObject::connect(var[2], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar2(int)));
+        QObject::connect(var[3], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar3(int)));
+        QObject::connect(var[4], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar4(int)));
+        QObject::connect(var[5], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar5(int)));
+        QObject::connect(var[6], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar6(int)));
+        QObject::connect(var[7], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar7(int)));
+        QObject::connect(var[8], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar8(int)));
+        QObject::connect(var[9], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar9(int)));
+        QObject::connect(var[10], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar10(int)));
+        QObject::connect(var[11], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar11(int)));
+        QObject::connect(var[12], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar12(int)));
+        QObject::connect(var[13], SIGNAL(valueChanged(int)),
+                    this, SLOT(modifyParameterVar13(int)));
+
+        //init linkNames which reprensent every robot link
+        linkNames[0]= "Torso";
+        linkNames[1]= "Neck";
+        linkNames[2]= "Head";
+        linkNames[3]= "LShoulderPitch";
+        linkNames[4]= "LShoulderRoll";
+        linkNames[5]= "LElbow";
+        linkNames[6]= "LHipYaw";
+        linkNames[7]= "LHipRoll";
+
+        linkNames[8]= "LHipPitch";
+        linkNames[9]= "LKnee";
+        linkNames[10]= "LAnklePitch";
+        linkNames[11]= "LAnkleRoll";
+        linkNames[12]= "RShoulderPitch";
+        linkNames[13]= "RShoulderRoll";
+        linkNames[14]= "RElbow";
+        linkNames[15]= "RHipYaw";
+        linkNames[16]= "RHipRoll";
+        linkNames[17]= "RHipPitch";
+        linkNames[18]= "RKnee";
+        linkNames[19]= "RAnklePitch";
+        linkNames[20]= "RAnkleRoll";
+
 }
 
+
+void PanelView::updateRobotAngle0(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle0");
+
+}
+
+void PanelView::updateRobotAngle1(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle1");
+
+}
+void PanelView::updateRobotAngle2(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle2");
+
+}
+void PanelView::updateRobotAngle3(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle3");
+
+}
+void PanelView::updateRobotAngle4(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle4");
+
+}
+void PanelView::updateRobotAngle5(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle5");
+
+}
+void PanelView::updateRobotAngle6(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle6");
+
+}
+void PanelView::updateRobotAngle7(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle7");
+
+}
+void PanelView::updateRobotAngle8(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle8");
+
+}
+void PanelView::updateRobotAngle9(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle9");
+
+}
+void PanelView::updateRobotAngle10(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle10");
+
+}
+void PanelView::updateRobotAngle11(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle11");
+
+}
+void PanelView::updateRobotAngle12(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle12");
+
+}
+void PanelView::updateRobotAngle13(int value){
+
+    qDebug(">>>>>chenrui>>>>updateRobotAngle13");
+
+}
+
+
+void PanelView::modifyParameterVar0( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar0");
+
+}
+
+void PanelView::modifyParameterVar1( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar1");
+
+}
+void PanelView::modifyParameterVar2( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar2");
+
+}
+void PanelView::modifyParameterVar3( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar3");
+
+}
+void PanelView::modifyParameterVar4( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar4");
+
+}
+void PanelView::modifyParameterVar5( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar5");
+
+}
+void PanelView::modifyParameterVar6( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar6");
+
+}
+void PanelView::modifyParameterVar7( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar7");
+
+}
+void PanelView::modifyParameterVar8( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar8");
+
+}
+void PanelView::modifyParameterVar9( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar9");
+
+}
+void PanelView::modifyParameterVar10( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar10");
+
+}
+void PanelView::modifyParameterVar11( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar11");
+
+}
+void PanelView::modifyParameterVar12( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar12");
+
+}
+void PanelView::modifyParameterVar13( int value){
+
+    qDebug(">>>>>chenrui>>>modifyParameterVar13");
+
+}
+
+
+void PanelView::UpdateRobot(QString linkname, int valuH){
+
+
+
+
+
+
+
+}
 void PanelView::update(){
 
     QPainter p(this);
@@ -125,7 +359,6 @@ void PanelView::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
    
-    qDebug(">>>>>>>PanelView::paintEvent>>>>");
 
     QPainter p(this);
     p.setPen(Qt::NoPen);
@@ -136,11 +369,9 @@ void PanelView::paintEvent(QPaintEvent *event)
 
 void PanelView::mousePressEvent(QMouseEvent* event)
 {
-    qDebug("PanelView::mousePressEvent");
 }
 
 void PanelView::mouseMoveEvent(QMouseEvent *event){
-    qDebug()<<"mouseMoveEvent:";
 }
 
 
